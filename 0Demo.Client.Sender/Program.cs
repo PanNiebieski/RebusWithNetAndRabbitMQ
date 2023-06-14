796414297b300cb8361ecd5e550894981ea9127a
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2023-06-14 15:24:24 +0200

    Add project files.

---
+++ 0Demo.Client.Sender/Program.cs
@@ -0,0 +1,22 @@
+﻿using Rebus.Activation;
+using Rebus.Config;
+
+using (var activator = new BuiltinHandlerActivator())
+{
+    var bus = Configure.With(activator)
+        .Logging(l => l.ColoredConsole())
+        .Transport(t => t.UseRabbitMq("amqp://guest:guest@localhost:5672", "0DemoRebus"))
+        .Start();
+
+    await Task.Delay(500);
+
+    while (true)
+    {
+        Console.Write("Type something >");
+        var text = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(text)) break;
+
+        bus.Publish(text).Wait();
+    }
+}
---
+++ 0Demo.Server.Reciver/Program.cs
@@ -0,0 +1,17 @@
+﻿using Rebus.Activation;
+using Rebus.Config;
+
+using (var act = new BuiltinHandlerActivator())
+{
+    act.Handle<string>(async message =>
+    {
+        Console.WriteLine($"Got message : {message}");
+    });
+
+    Configure.With(act)
+        .Transport(t => t.UseRabbitMq("amqp://guest:guest@localhost:5672", "0DemoRebus"))
+        .Start();
+
+    Console.WriteLine("Press enter to quit");
+    Console.ReadLine();
+}
